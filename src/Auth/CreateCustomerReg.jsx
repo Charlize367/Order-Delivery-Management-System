c35910fb9e968614cd3ee6581e1f51718449d3a1
Charlize367 <charlizecmendoza@gmail.com>
2025-08-13 20:51:12 +0800

    Add files via upload
    - Already connected to Springboot API to add customers (register)

--- src/Auth/CreateCustomerReg.jsx
+++ src/Auth/CreateCustomerReg.jsx
@@ -1,11 +1,74 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export const CreateCustomerReg = () => {
-  return (
-    <div>CreateCustomerReg
-    </div>
-  )
+
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(username);
+
+        if(password === confirmPassword) {
+            try {
+                const postData = {
+                    "username" : username,
+                    "password" : password,
+                    "enabled" : true,
+                    "role" : "CUSTOMER"
+                }
+                const response = await axios.post('http://localhost:8083/users/register', postData, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+                console.log(response);
+                return true;
+
+            } catch (error) {
+                console.error('Account Creation failed:', error);
+                window.alert('Account Creation failed');
+                return false;
+            }
+          } else {
+            window.alert("Passwords do not match.");
+          }
+
+        }
+
+
+    return(
+        <section className="login-body">
+        <div className="form-container">
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+            <input className="fields" type="text" placeholder="Username" value={username} onChange={handleUsernameChange}  />
+            <input className="fields" type="password" placeholder="Password" value={password} onChange={handlePasswordChange}/>
+            <input className="fields" type="password" placeholder="Password" value={confirmPassword} onChange={handleConfirmPasswordChange}/>
+            <input className="loginBtn" type="submit" value="Create Account" />
+            </form>
+        </div>
+</section>
+    )
+
 }
+
 
 export default CreateCustomerReg;
